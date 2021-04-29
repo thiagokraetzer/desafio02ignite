f9215ba87db8ee3ed7ba7dcdb106f8d92b55b905
root <thiago.kraetzer@brainweb.com.br>
2021-04-29 09:44:18 -0300

    Finish: finishing exercise

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import { Button } from './Button';
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface SideBarProps {
+  genres: Genre[];
+  handleOpenGenre(id: number): void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, selectedGenreId, handleOpenGenre }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleOpenGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
+}
